f04c7713d6960a4f8874698b6b9fe6007828bf23
Mogakamo <mogaka.amo254@gmail.com>
2021-07-04 15:33:59 +0300

    Enabled the git for my project

---
+++ src/main/java/com/example/mogaka_dev/MogakaDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.mogaka_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MogakaDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MogakaDevApplication.class, args);
+	}
+
+}
